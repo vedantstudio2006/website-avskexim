7914cedb6ef73ccf1082363c85fa9de0e1048bbc
blue-casper <micjin092@gmail.com>
2026-03-27 22:23:52 +0530

    add the react bit background to the contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,6 @@
-import "./Contact.css"
+import "./Contact.css";
+import Particles from './Particles';
+
 import { useScrollToHash } from "../utils/NavigatePage";
 import { useState } from "react";
 import { registerUser } from "../utils/FormSubmit";
@@ -52,6 +54,19 @@ export const Contact = () => {
 
   return (
     <>
+      <div style={{ width: "100%", height: "600px", position: "relative" }}>
+        <Particles
+          particleColors={["#ffffff"]}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover
+          alphaParticles={false}
+          disableRotation={false}
+          pixelRatio={1}
+        />
+      </div>
       <div id="form">
         <div className="signup-container">
           <form className="signup-form" onSubmit={handleSubmit}>
